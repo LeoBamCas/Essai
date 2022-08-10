53f45d7ed841b1a7644562d9f07c136a53719628
LeoBamCas <100698966+LeoBamCas@users.noreply.github.com>
2022-08-10 14:24:05 +0200

    Header (#3)
    feat: header

---
+++ src/component/header.php
@@ -0,0 +1,24 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <div id="logo_poisson">
+                    <img src="public/img/logo_poisson.png" alt="logo d'overflow guy avec tête de poisson">
+                </div>
+            </div>
+            <div class="col-4">
+            </div>
+            <div class="col-6 align-self-end">
+                <nav>
+                    <ul>
+                        <li><a href="#">Accueil </a></li>
+                        <li><a href="#"> Présentation </a></li>
+                        <li><a href="#"> Historique </a></li>
+                        <li><a href="multimedia.html"> Multimédia </a></li>
+                        <li><a href="#"> Contact </a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
