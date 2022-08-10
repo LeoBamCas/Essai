657ae3c6abbc2ebce209c7ef5bef37de72c8586f
Gwendal <ganquetil62@gmail.com>
2022-08-10 13:39:41 +0200

    feat: footer

---
+++ src/component/footer.php
@@ -0,0 +1,53 @@
+<!-- <footer>
+    <div id="reseaux_sociaux">
+        <h1>Reseaux sociaux</h1>
+        <a href="https://www.facebook.com/Overflowguy"><img src="../public/img/logofb.png" height="70px" alt="logo fb" class="logofb" /></a>
+        <a href="https://overflowguy.bandcamp.com/album/what-now"><img src="../public/img/logobandcamp.png" height="70px" alt="logo bandcamp" class="logo bandcamp" /></a>
+        <br />
+        <a href="https://www.youtube.com/channel/UCBZCRzvvBmTgGD8qEVFUrEw"><img src="../public/img/logoyt.png" height="70px" alt="logo you tube" class="logo_you_tube" /></a>
+        <a href="https://www.instagram.com/overflowguy/?hl=fr"><img src="../public/img/instagram.png" height="75px" width="70px" alt="logo instagram" class="logo_instagram"></a>
+    </div>
+    <div id="copains">
+        <h1 class="titraside">Ceux qui nous aident et qu'on les aime : </h1>
+        <ul class="listecopains1">
+            <li><a href="https://malauxyeux.bandcamp.com/">Mal aux yeux</a></li>
+            <li><a href="https://vuparben.bandcamp.com/">Vu par Ben</a></li>
+            <li><a href="https://www.tumblr.com/tagged/tournebouluche?sort=recent">Tournebouluche</a></li>
+        </ul>
+        <ul class="listecopains2">
+            <li><a href="https://associationleforum.fr/">L'association Le forum</a></li>
+            <li><a href="https://www.facebook.com/deltadivinetheband/">Delta Divine</a></li>
+            <li><a href="https://www.facebook.com/centreculturel">Le centre culturel de Beauraing</a></li>
+        </ul>
+    </div>
+</footer> -->
+
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <h1>Reseaux sociaux</h1>
+            </div>
+            <div class="col-3">
+            </div>
+            <div class="col-5">
+                <h1 class="titraside">Ceux qui nous aident et qu'on les aime : </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                <ul>
+                    <li><a href="https://www.facebook.com/Overflowguy"><img src="../public/img/logofb.png" height="70px" alt="logo fb" class="logofb" /></a></li>
+                    <li><a href="https://overflowguy.bandcamp.com/album/what-now"><img src="../public/img/logobandcamp.png" height="70px" alt="logo bandcamp" class="logo bandcamp" /></a></li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <ul>
+                    <li><a href="https://www.youtube.com/channel/UCBZCRzvvBmTgGD8qEVFUrEw"><img src="../public/img/logoyt.png" height="70px" alt="logo you tube" class="logo_you_tube" /></a></li>
+                    <li><a href="https://www.instagram.com/overflowguy/?hl=fr"><img src="../public/img/instagram.png" height="75px" width="70px" alt="logo instagram" class="logo_instagram"></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
