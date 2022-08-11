c7bba856b7be5ad09c531afb06078db05c9bdf2a
Gwendal <ganquetil62@gmail.com>
2022-08-11 16:26:50 +0200

    feat: debut refonte bootstrap

--- src/component/presentationsection.php
+++ src/component/presentationsection.php
@@ -1,34 +1,79 @@
 <section>
+    <!--<article>
+        <h1>Presentation </h1>
+        <img src="public/img/photo_article.jpg" alt="bob et gwen dans l'herbe" class="herbe" />
+        <div class="ap1">Overflow Guy essaye d'être un duo Noise-grunge aux accents psychédéliques avec une légère touche de progressif.
+            Vraiment légère, hein !<br />
+            Née de la frustration du premier confinement, la musique d'Overflow guy se veut directe, profonde, subtile et insaisissable.
+            Aucun effort ne sera fait pour cacher les influences des années 90, pourtant les auditeur.ices auront bien du mal à définir
+            précisément le style du groupe tout en l'ayant sur le bout de la langue.</div>
+        <img src="public/img/coesette.jpg" alt="concert_coesette" class="coesette" />
+        <div class="ap2"> Disons qu'avec des mélodies à la fois très simples et complexes, un son lourd et fragile et une énergie ambiante,
+            Overflow guy tente d'explorer et de mélanger les différentes atmosphères de nombreuses catégories de rock alternatif.</div>
+    </article>
 
-			<article>
-				
-				<h1><a href="#"><img src="public/img/photo_article.jpg" alt="bob et gwen dans l'herbe" class="herbe"/></a> Presentation  </h1>
-				
-					<div class="ap1">Overflow Guy essaye d'être un duo Noise-grunge aux accents psychédéliques avec une légère touche de progressif. Vraiment légère, hein !<br/>
-					Née de la frustration du premier confinement, la musique d'Overflow guy se veut directe, profonde, subtile et insaisissable. Aucun effort ne sera fait pour cacher les influences des années 90, pourtant les auditeur.ices auront bien du mal à définir précisément le style du groupe tout en l'ayant sur le bout de la langue.</div>
+    <aside>
+        <div class="asidebob">
+            <h2>Bob,<br /> guitare et voix lead</h2>
+            <p>Bob mange des mini-beignets. Il paraîtrait même que quand il passe à aldi, il n'en reste JAMAIS.<br />Parfois il joue de
+                la guitare et torture son bigsby avec la rage d'un clebard de stalker (le jeu, pas le film) en beuglant tout son saoul entre
+                deux miaulement que ne renierait pas un thom yorke perdu au fond d'une contrebasse.</p>
+            <img src="public/img/saybobtodeterminism.jpg" alt="bob avec un ventilateur" class="saybobto" />
+        </div>
+        <div class="asidegwen">
+            <h2>Gwen,<br /> batterie et voix lointaine</h2>
+            <p>Gwen mange des croissants aux amandes et incite les mécréants à boire de la boisson énergisante bon marché.De temps en temps il
+                fait résonner des toms accordés presque avec soin et claquer des cymbales à faire vomir les sirènes. Celles du stade bollaert, pas
+                les femmes volantes cannibales.</p>
+            <img src="public/img/fauxgwen.jpg" alt="gwen on fire" class="fauxgwen" />
+        </div>
+    </aside> -->
 
-					<img src="public/img/coesette.jpg" alt="concert_coesette" class="coesette"/> 
-					<div class="ap2"> Disons qu'avec des mélodies à la fois très simples et complexes, un son lourd et fragile et une énergie ambiante, Overflow guy tente d'explorer et de mélanger les différentes atmosphères de nombreuses catégories de rock alternatif.</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-1">
 
-				
-				
-			</article>
+            </div>
+            <div class="col-5">
+                <h1> Qu'est ce qu'Overflow Guy ?<h1>
+            </div>
+            <div class="col-2">
 
-			<aside>
-				<div class="asidebob">
-					<h2>Bob,<br/> guitare et voix lead</h2>
-					<p>Bob mange des mini-beignets. Il paraîtrait même que quand il passe à aldi, il n'en reste JAMAIS.<br/>Parfois il joue de la guitare et torture son bigsby avec la rage d'un clebard de stalker (le jeu, pas le film) en beuglant tout son saoul entre deux miaulement que ne renierait pas un thom yorke perdu au fond d'une contrebasse.</p>
-					<img src="public/img/saybobtodeterminism.jpg" alt="bob avec un ventilateur" class="saybobto"/>
-					
+            </div>
+            <div class="col-4">
+                <h1>Qui sont Overflow Guy ?</h1>
+            </div>
 
-				</div>
-				<div class="asidegwen">
-					<h2>Gwen,<br/> batterie et voix lointaine</h2>
-				
-					<p>Gwen mange des croissants aux amandes et incite les mécréants à boire de la boisson énergisante bon marché.De temps en temps il fait résonner des toms accordés presque avec soin et claquer des cymbales à faire vomir les sirènes. Celles du stade bollaert, pas les femmes volantes cannibales.</p> 
-					<img src="public/img/fauxgwen.jpg" alt="gwen on fire" class="fauxgwen"/>
-				</div>
+        </div>
+        <div class="row">
+            <div class="col-5">
+                <article>
+                    <div class="ap1">Overflow Guy essaye d'être un duo Noise-grunge aux accents psychédéliques avec une légère touche de progressif.
+                        Vraiment légère, hein !<br />
+                        Née de la frustration du premier confinement, la musique d'Overflow guy se veut directe, profonde, subtile et insaisissable.
+                        Aucun effort ne sera fait pour cacher les influences des années 90, pourtant les auditeur.ices auront bien du mal à définir
+                        précisément le style du groupe tout en l'ayant sur le bout de la langue.</div>
+                    </div>
+                </article>
+            <div class="col-3">
+                <img src="public/img/photo_article.jpg" alt="bob et gwen dans l'herbe" class="herbe" />
+            </div>
+            </div>
+        <div class="row">
+            <div class="col-3">
+            <img src="public/img/coesette.jpg" alt="concert_coesette" class="coesette" />
+            </div>
+            <div class="col-1">
+                
+            </div>
+            <div class="col-4">
+                <article>
+                <div class="ap2"> Disons qu'avec des mélodies à la fois très simples et complexes, un son lourd et fragile et une énergie ambiante,
+                    Overflow guy tente d'explorer et de mélanger les différentes atmosphères de nombreuses catégories de rock alternatif.</div>
+                </article>
+            </div>
 
-			</aside>
-
-		</section>
+        </div>
+    </div>
+    </div>
+</section>
