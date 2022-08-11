0f9b40ceac55ec0dd0b82ada0f3aea23838bcc09
Gwendal <ganquetil62@gmail.com>
2022-08-11 16:58:16 +0200

    feat: insertion des dernieres infos

--- src/component/presentationsection.php
+++ src/component/presentationsection.php
@@ -1,4 +1,3 @@
-<section>
     <!--
     <aside>
         <div class="asidebob">
@@ -16,7 +15,7 @@
             <img src="public/img/fauxgwen.jpg" alt="gwen on fire" class="fauxgwen" />
         </div>
     </aside> -->
-
+<section>
     <div class="container">
         <div class="row">
             <div class="col text-center">
@@ -63,7 +62,30 @@
             </div>
         </div>
         <div class="row">
-            
+            <div class="col text-center">
+                <h2>Bob,<br /> guitare et voix lead</h2>
+            </div>
+            <div class="col text-center">
+                <h2>Gwen,<br /> batterie et voix lointaine</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>Bob mange des mini-beignets. Il paraîtrait même que quand il passe à aldi, il n'en reste JAMAIS.<br />Parfois il joue de
+                    la guitare et torture son bigsby avec la rage d'un clebard de stalker (le jeu, pas le film) en beuglant tout son saoul entre
+                    deux miaulement que ne renierait pas un thom yorke perdu au fond d'une contrebasse.</p>
+            </div>
+            <div class="col">
+                <img src="public/img/saybobtodeterminism.jpg" alt="bob avec un ventilateur" class="saybobto" />
+            </div>
+            <div class="col">
+                <img src="public/img/fauxgwen.jpg" alt="gwen on fire" class="fauxgwen" />
+            </div>
+            <div class="col">
+                <p>Gwen mange des croissants aux amandes et incite les mécréants à boire de la boisson énergisante bon marché.De temps en temps il
+                    fait résonner des toms accordés presque avec soin et claquer des cymbales à faire vomir les sirènes. Celles du stade bollaert, pas
+                    les femmes volantes cannibales.</p>
+            </div>
         </div>
     </div>
     </div>
