711875d0446bd1b69c29f6cf7470ec1b19760ff8
Gwendal <ganquetil62@gmail.com>
2022-08-11 15:42:17 +0200

    feat: miseenplacepresentation

---
+++ src/component/presentationsection.php
@@ -0,0 +1,34 @@
+<section>
+
+			<article>
+				
+				<h1><a href="#"><img src="public/img/photo_article.jpg" alt="bob et gwen dans l'herbe" class="herbe"/></a> Presentation  </h1>
+				
+					<div class="ap1">Overflow Guy essaye d'être un duo Noise-grunge aux accents psychédéliques avec une légère touche de progressif. Vraiment légère, hein !<br/>
+					Née de la frustration du premier confinement, la musique d'Overflow guy se veut directe, profonde, subtile et insaisissable. Aucun effort ne sera fait pour cacher les influences des années 90, pourtant les auditeur.ices auront bien du mal à définir précisément le style du groupe tout en l'ayant sur le bout de la langue.</div>
+
+					<img src="public/img/coesette.jpg" alt="concert_coesette" class="coesette"/> 
+					<div class="ap2"> Disons qu'avec des mélodies à la fois très simples et complexes, un son lourd et fragile et une énergie ambiante, Overflow guy tente d'explorer et de mélanger les différentes atmosphères de nombreuses catégories de rock alternatif.</div>
+
+				
+				
+			</article>
+
+			<aside>
+				<div class="asidebob">
+					<h2>Bob,<br/> guitare et voix lead</h2>
+					<p>Bob mange des mini-beignets. Il paraîtrait même que quand il passe à aldi, il n'en reste JAMAIS.<br/>Parfois il joue de la guitare et torture son bigsby avec la rage d'un clebard de stalker (le jeu, pas le film) en beuglant tout son saoul entre deux miaulement que ne renierait pas un thom yorke perdu au fond d'une contrebasse.</p>
+					<img src="public/img/saybobtodeterminism.jpg" alt="bob avec un ventilateur" class="saybobto"/>
+					
+
+				</div>
+				<div class="asidegwen">
+					<h2>Gwen,<br/> batterie et voix lointaine</h2>
+				
+					<p>Gwen mange des croissants aux amandes et incite les mécréants à boire de la boisson énergisante bon marché.De temps en temps il fait résonner des toms accordés presque avec soin et claquer des cymbales à faire vomir les sirènes. Celles du stade bollaert, pas les femmes volantes cannibales.</p> 
+					<img src="public/img/fauxgwen.jpg" alt="gwen on fire" class="fauxgwen"/>
+				</div>
+
+			</aside>
+
+		</section>
