31c41778f2b7c20de013c2b1ca087b015871e499
Gwendal <ganquetil62@gmail.com>
2022-08-11 15:22:18 +0200

    fix: fotter en bas de page

--- src/index.php
+++ src/index.php
@@ -9,8 +9,8 @@
 </head>
 <body>
 	<?php require_once "component/header.php"; ?>
-	<?php require_once "component/footer.php"; ?>
 	<?php require_once "component/carousel.php"; ?>
+	<?php require_once "component/footer.php"; ?>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
