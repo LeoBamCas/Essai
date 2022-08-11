67ac91482f102f47b36b84a42f60f357d0f760b4
Gwendal <ganquetil62@gmail.com>
2022-08-11 10:56:33 +0200

    feat:mise en place carousel

---
+++ src/component/carousel.php
@@ -0,0 +1,13 @@
+<div class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="img-fluid d-block w-100" src="public/img/photo_beauraing_grand.jpg" alt="Premier slide">
+        </div>
+        <div class="carousel-item">
+            <img class="img-fluid d-block w-100" src="public/img/bobratperche.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="img-fluid d-block w-100" src="public/img/gwenratperche.jpg" alt="Third slide">
+        </div>
+    </div>
+</div>
