97614d865e554680337a5026d084d427f5393a40
Gwendal <ganquetil62@gmail.com>
2022-08-11 11:04:29 +0200

    fix:légende au carousel

--- src/component/carousel.php
+++ src/component/carousel.php
@@ -2,12 +2,21 @@
     <div class="carousel-inner">
         <div class="carousel-item active">
             <img class="img-fluid d-block w-100" src="public/img/photo_beauraing_grand.jpg" alt="Premier slide">
+            <div class="carousel-caption d-none d-md-block">
+                <p>Showcase à Beauraing (be) en juillet 2021</p>
+            </div>
         </div>
         <div class="carousel-item">
             <img class="img-fluid d-block w-100" src="public/img/bobratperche.jpg" alt="Second slide">
+            <div class="carousel-caption d-none d-md-block">
+                <p>Concert à Arras, au rat perché, en juillet 2022</p>
+            </div>
         </div>
         <div class="carousel-item">
             <img class="img-fluid d-block w-100" src="public/img/gwenratperche.jpg" alt="Third slide">
+            <div class="carousel-caption d-none d-md-block">
+                <p>Concert à Arras, au rat perché, en juillet 2022</p>
+            </div>
         </div>
     </div>
 </div>
