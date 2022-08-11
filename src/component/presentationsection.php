23b1d8a0409c92da068207600ce18b125e975b46
Gwendal <ganquetil62@gmail.com>
2022-08-11 16:46:14 +0200

    fix: changement de mise en page

--- src/component/presentationsection.php
+++ src/component/presentationsection.php
@@ -1,17 +1,5 @@
 <section>
-    <!--<article>
-        <h1>Presentation </h1>
-        <img src="public/img/photo_article.jpg" alt="bob et gwen dans l'herbe" class="herbe" />
-        <div class="ap1">Overflow Guy essaye d'être un duo Noise-grunge aux accents psychédéliques avec une légère touche de progressif.
-            Vraiment légère, hein !<br />
-            Née de la frustration du premier confinement, la musique d'Overflow guy se veut directe, profonde, subtile et insaisissable.
-            Aucun effort ne sera fait pour cacher les influences des années 90, pourtant les auditeur.ices auront bien du mal à définir
-            précisément le style du groupe tout en l'ayant sur le bout de la langue.</div>
-        <img src="public/img/coesette.jpg" alt="concert_coesette" class="coesette" />
-        <div class="ap2"> Disons qu'avec des mélodies à la fois très simples et complexes, un son lourd et fragile et une énergie ambiante,
-            Overflow guy tente d'explorer et de mélanger les différentes atmosphères de nombreuses catégories de rock alternatif.</div>
-    </article>
-
+    <!--
     <aside>
         <div class="asidebob">
             <h2>Bob,<br /> guitare et voix lead</h2>
@@ -31,48 +19,51 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-1">
-
-            </div>
-            <div class="col-5">
+            <div class="col text-center">
                 <h1> Qu'est ce qu'Overflow Guy ?<h1>
             </div>
-            <div class="col-2">
-
-            </div>
-            <div class="col-4">
-                <h1>Qui sont Overflow Guy ?</h1>
-            </div>
-
         </div>
         <div class="row">
-            <div class="col-5">
+            <div class="col-1">
+            </div>
+            <div class="col">
                 <article>
                     <div class="ap1">Overflow Guy essaye d'être un duo Noise-grunge aux accents psychédéliques avec une légère touche de progressif.
                         Vraiment légère, hein !<br />
                         Née de la frustration du premier confinement, la musique d'Overflow guy se veut directe, profonde, subtile et insaisissable.
                         Aucun effort ne sera fait pour cacher les influences des années 90, pourtant les auditeur.ices auront bien du mal à définir
-                        précisément le style du groupe tout en l'ayant sur le bout de la langue.</div>
+                        précisément le style du groupe tout en l'ayant sur le bout de la langue.
                     </div>
-                </article>
-            <div class="col-3">
+            </div>
+            </article>
+            <div class="col">
                 <img src="public/img/photo_article.jpg" alt="bob et gwen dans l'herbe" class="herbe" />
             </div>
-            </div>
-        <div class="row">
-            <div class="col-3">
-            <img src="public/img/coesette.jpg" alt="concert_coesette" class="coesette" />
-            </div>
             <div class="col-1">
-                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-1">
+            </div>
+            <div class="col">
+                <img src="public/img/coesette.jpg" alt="concert_coesette" class="coesette" />
             </div>
             <div class="col-4">
                 <article>
-                <div class="ap2"> Disons qu'avec des mélodies à la fois très simples et complexes, un son lourd et fragile et une énergie ambiante,
-                    Overflow guy tente d'explorer et de mélanger les différentes atmosphères de nombreuses catégories de rock alternatif.</div>
+                    <div class="ap2"> Disons qu'avec des mélodies à la fois très simples et complexes, un son lourd et fragile et une énergie ambiante,
+                        Overflow guy tente d'explorer et de mélanger les différentes atmosphères de nombreuses catégories de rock alternatif.</div>
                 </article>
             </div>
-
+            <div class="col-1">
+            </div>
+        </div>
+        <div  class="row">
+            <div class="col text-center">
+                <h1>Mais qui sont Overflow Guy ?</h1>
+            </div>
+        </div>
+        <div class="row">
+            
         </div>
     </div>
     </div>
