48444778fa88b1f64fcac03680647067f0b39ff3
Gwendal <ganquetil62@gmail.com>
2022-08-12 14:03:09 +0200

    feat: termine section videos

--- src/component/video.php
+++ src/component/video.php
@@ -5,18 +5,18 @@
         </div>
     </div>
     <div class="row">
-        <div class="col" class="video">
+        <div class="col video" >
             <iframe width="560"  height="315" src="https://www.youtube.com/embed/3ilbaN87yHE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
-        <div class="col" class="video">
+        <div class="col video" >
             <iframe width="560"  height="315" src="https://www.youtube.com/embed/GenF5AYtSxc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </div>
     <div class="row">
-        <div class="col" class="video">
+        <div class="col video" >
             <iframe width="560"  height="315" src="https://www.youtube.com/embed/cNun5WHtKGM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
-        <div class="col" class="video">
+        <div class="col video" >
             <iframe width="560"  height="315" src="https://www.youtube.com/embed/0V4uJzlvFPQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </div>
