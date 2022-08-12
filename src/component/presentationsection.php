818f55c840294a62370ebed1504405df78469233
Gwendal <ganquetil62@gmail.com>
2022-08-12 10:29:50 +0200

    fix:grille mais qui sont

--- src/component/presentationsection.php
+++ src/component/presentationsection.php
@@ -52,22 +52,22 @@
                 <h2>Bob,<br /> guitare et voix lead</h2>
             </div>
         </div>
-        <div class="row justify-content-center">
-            <div class="article">
-                <div class="col">
+        <div class="row">
+                <div class="col-4">
                     <p class="gens">Gwen mange des croissants aux amandes et incite les mécréants à boire de la boisson énergisante bon marché.De temps en temps il
                         fait résonner des toms accordés presque avec soin et claquer des cymbales à faire vomir les sirènes. Celles du stade bollaert, pas
                         les femmes volantes cannibales.</p>
                 </div>
-                <div class="col">
+                <div class="col-1">
+                </div>
+                <div class="col-3 justify-content-center">
                     <img src="public/img/qui_sont.jpg" alt="bob avec un ventilateur" class="quisont" />
                 </div>
-                <div class="col">
+                <div class="col-4">
                     <p class="gens">Bob mange des mini-beignets. Il paraîtrait même que quand il passe à aldi, il n'en reste JAMAIS.<br />Parfois il joue de
                         la guitare et torture son bigsby avec la rage d'un clebard de stalker (le jeu, pas le film) en beuglant tout son saoul entre
                         deux miaulement que ne renierait pas un thom yorke perdu au fond d'une contrebasse.</p>
                 </div>
-            </div>
         </div>
     </div>
 </section>
