0fa3522c7d98b9da5b336aec35792c9cdee61eb3
Gwendal <ganquetil62@gmail.com>
2022-08-12 15:39:50 +0200

    feat: mise en place formulaire de contact

---
+++ src/component/contactsection.php
@@ -0,0 +1,9 @@
+<section>
+    <h2>pour toute demande d'information veuillez nous laisser un message ci dessous</h2>
+    <p>ou nous contacter directement sur <a url="mailto:overflow.guy@lavache.com">overflow.guy@lavache.com</a></p>
+    <form method="$_POST">
+        <input type="text" name="Nom" placeholder="Votre Nom">
+        <input type="email" name="email" placeholder="Votre email">
+        <textarea>Pour du booking, du soutien ou mêmes des critiques</textarea>
+    </form>
+</section>
