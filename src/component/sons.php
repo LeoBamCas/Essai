623c46b138f6adfe82b65b2c8b819e8dcc12dcd9
Gwendal <ganquetil62@gmail.com>
2022-08-12 14:17:55 +0200

    feat: ajout section sons

---
+++ src/component/sons.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class="row">
+        <div class="col text-center">
+            <h1>SONS</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=1142612813/size=large/bgcol=ffffff/linkcol=7137dc/tracklist=false/transparent=true/" seamless><a href="https://overflowguy.bandcamp.com/album/what-now">What now ? by Overflow Guy</a></iframe>
+        </div>
+    <div class="row">
+        <div class="col">
+            <audio src="public/sound/purge2.wav" controls></audio>
+        </div>
+    </div>
+    </div>
+</div>
