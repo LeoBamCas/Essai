1f05149000a691468580dcea122a6fa851eceb5e
Gwendal <ganquetil62@gmail.com>
2022-08-12 16:18:48 +0200

    fix: premiere version contact

--- src/component/contactsection.php
+++ src/component/contactsection.php
@@ -1,9 +1,31 @@
-<section>
-    <h2>pour toute demande d'information veuillez nous laisser un message ci dessous</h2>
-    <p>ou nous contacter directement sur <a url="mailto:overflow.guy@lavache.com">overflow.guy@lavache.com</a></p>
-    <form method="$_POST">
-        <input type="text" name="Nom" placeholder="Votre Nom">
-        <input type="email" name="email" placeholder="Votre email">
-        <textarea>Pour du booking, du soutien ou mêmes des critiques</textarea>
-    </form>
-</section>
+<br />
+<br />
+<div class="container">
+    <div class="row">
+        <div class="col text-center">
+            <h2>pour toute demande d'information veuillez nous laisser un message ci dessous</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col text-center">
+            <p>ou nous contacter directement sur <a url="mailto:overflow.guy@lavache.com">overflow.guy@lavache.com</a></p>
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col text-center">
+            <form method="$_POST">
+                <input type="text" name="Nom" placeholder="Votre Nom">
+                <br />
+                <br />
+                <input type="email" name="email" placeholder="Votre email">
+                <br />
+                <br />
+                <textarea rows="15">Pour du booking, du soutien ou mêmes des critiques</textarea>
+            </form>
+        </div>
+    </div>
+
+</div>
+<br />
+<br />
